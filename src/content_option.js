04a34c54ea80cd393b6b0c8236ab4e2f30d4c075
coderwenwen <wangwenusa11@gmail.com>
2025-06-01 16:40:20 -0700

    local is working

--- src/content_option.js
+++ src/content_option.js
@@ -1,18 +1,18 @@
-const logotext = "JOHN";
+const logotext = "WW";
 const meta = {
-    title: "John Doe",
+    title: "Wendy Wang",
     description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
 };
 
 const introdata = {
-    title: "I’m John Doe",
+    title: "I’m Wendy Wang",
     animated: {
         first: "I love coding",
         second: "I code cool websites",
         third: "I develop mobile apps",
     },
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
-    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+    your_img_url: "/headshot.jpg",
 };
 
 const dataabout = {
